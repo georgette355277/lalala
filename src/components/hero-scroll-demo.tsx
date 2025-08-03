e7669f9f8acdbad486fdc0b9ba649cd1ffd95e94
georgette355277 <georgette355277@gmail.com>
2025-08-03 13:47:44 +0200

    Make tablet bigger and remove home button

--- src/components/hero-scroll-demo.tsx
+++ src/components/hero-scroll-demo.tsx
@@ -14,8 +14,8 @@ export function HeroScrollDemo() {
         <div 
           className="relative bg-gray-800 rounded-[2rem] sm:rounded-[2.5rem] p-3 sm:p-4 shadow-2xl"
           style={{
-            width: 'min(95vw, 1100px)',
-            height: 'min(60vh, 700px)',
+            width: 'min(95vw, 1400px)',
+            height: 'min(70vh, 900px)',
             boxShadow: '0 25px 50px -12px rgba(0, 0, 0, 0.8), inset 0 1px 0 rgba(255, 255, 255, 0.1)'
           }}
         >
@@ -32,13 +32,6 @@ export function HeroScrollDemo() {
               <ShinyButton onClick={() => setIsModalOpen(true)}>
                 Mint
               </ShinyButton>
-            </div>
-          </div>
-          
-          {/* Home Button */}
-          <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2">
-            <div className="w-8 h-8 sm:w-12 sm:h-12 bg-gray-700 rounded-full flex items-center justify-center">
-              <div className="w-4 h-4 sm:w-6 sm:h-6 bg-gray-500 rounded-full"></div>
             </div>
           </div>
         </div>
