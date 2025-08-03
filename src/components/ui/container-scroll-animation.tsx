e174f4307ca4405be8ee43f6bc36e480ca166b0b
georgette355277 <georgette355277@gmail.com>
2025-08-03 13:28:46 +0200

    Fix diff syntax accidentally included in file

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -1,7 +1,5 @@
-@@ .. @@
-         <div className="py-10 md:py-40 w-full relative">
--          <div className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl">
-+          <div className="-mt-12 mx-auto h-[30rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl">
+        <div className="py-10 md:py-40 w-full relative">
+          <div className="-mt-12 mx-auto h-[30rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl">
              {children}
            </div>
          </div>
