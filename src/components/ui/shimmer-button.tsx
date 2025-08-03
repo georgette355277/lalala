f30eb10816944b2cb9007feffbaa884df1ed52b8
georgette355277 <georgette355277@gmail.com>
2025-08-03 14:34:28 +0200

    Fix button text color to white

--- src/components/ui/shimmer-button.tsx
+++ src/components/ui/shimmer-button.tsx
@@ -60,7 +60,9 @@ const ShimmerButton = React.forwardRef<HTMLButtonElement, ShimmerButtonProps>(
             <div className="animate-spin-around absolute -inset-full w-auto rotate-0 [background:conic-gradient(from_calc(270deg-(var(--spread)*0.5)),transparent_0,var(--shimmer-color)_var(--spread),transparent_var(--spread))] [translate:0_0]" />
           </div>
         </div>
-        {children}
+        <span className="relative z-10 text-white font-medium">
+          {children}
+        </span>
 
         {/* Highlight */}
         <div
